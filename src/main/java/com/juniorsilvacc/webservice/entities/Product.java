4037df76ba693d103954e06728276621099506fb
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-10-20 14:42:46 -0300

    many-to-many association with jointable

--- src/main/java/com/juniorsilvacc/webservice/entities/Product.java
+++ src/main/java/com/juniorsilvacc/webservice/entities/Product.java
@@ -1,11 +1,16 @@
 package com.juniorsilvacc.webservice.entities;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +24,12 @@ public class Product {
 	private String description;
 	private double price;
 	private String imgUrl;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
 	}
@@ -70,6 +81,10 @@ public class Product {
 
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
+	}
+	
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
