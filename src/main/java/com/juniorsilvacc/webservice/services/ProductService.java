6eb82ed365701fa35fb7955ac8571a8a17a9bb3a
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-10-20 14:10:22 -0300

    product

---
+++ src/main/java/com/juniorsilvacc/webservice/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.juniorsilvacc.webservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juniorsilvacc.webservice.entities.Product;
+import com.juniorsilvacc.webservice.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+
+		return obj.get();
+	}
+
+}
