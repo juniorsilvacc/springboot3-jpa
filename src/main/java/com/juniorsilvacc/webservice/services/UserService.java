7db7664732757da268bbedef15711192b287c593
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-12-30 03:55:51 -0300

    Exception handling - findById

--- src/main/java/com/juniorsilvacc/webservice/services/UserService.java
+++ src/main/java/com/juniorsilvacc/webservice/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.juniorsilvacc.webservice.entities.User;
 import com.juniorsilvacc.webservice.repositories.UserRepository;
+import com.juniorsilvacc.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
